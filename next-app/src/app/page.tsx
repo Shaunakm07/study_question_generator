956582e2480813bbcdb98d7a4c6383b88d173b72
Shaunakm07 <137065822+Shaunakm07@users.noreply.github.com>
2025-08-05 06:03:14 +0100

    made the rendered page look a lot nicer for the user

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,10 +1,12 @@
 export default async function Home() {
     let data = await fetch('http://127.0.0.1:5000/api/data');
+    let posts = await data.json();
 
-    let posts = await data.json()
     return (
-        <ul>
-            {(posts.message)}
-        </ul>
-    )
+        <div className="min-h-screen bg-gradient-to-r from-gray-950 flex justify-center items-center bg-[linear-gradient(to_right,_#0a0a0a_67%,_#1a001f_100%)]">
+            <p className="text-white font-bold text-2xl">
+                {posts.message}
+            </p>
+        </div>
+    );
 }
